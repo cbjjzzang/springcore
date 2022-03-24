ebdf9472cc920890b280089a752997d0ce076ac5
Bongjin Choi <cbjjzzang@naver.com>
2022-03-24 17:48:28 +0900

    Initial commit

---
+++ src/main/java/com/sparta/springcore/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.sparta.springcore.security;
+
+import com.sparta.springcore.model.User;
+import com.sparta.springcore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //유저 이름을 가지고 유저 정보를 찾아 가져온다
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Can't find " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
